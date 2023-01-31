b14cfa1d2a0309b2db29d36982eaa1da0b063e98
Maria Clara <mariacnm2@al.insper.edu.br>
2023-01-31 11:06:08 -0300

    jogo finalizado

--- game_screen.py
+++ game_screen.py
@@ -2,7 +2,7 @@ import pygame
 from config import FPS, WIDTH, HEIGHT, BLACK
 from assets import carrega_arquivos
 from sprites import Caixa
-
+import random
 
 def game_screen(window):
     # Variável para o ajuste de velocidade
@@ -29,17 +29,18 @@ def game_screen(window):
     
     palavra=""
     ponto=0
+    listapalavra=['exceto', 'cinico',"banana","bala","batata","bateria","cacau","cadeado", "cadeira", "cafe" ,"cafune","domado","domino","doze","duvida","duzia", 'idoneo', 'ambito', 'nescio', 'exceto', 'cinico', 'idoneo', 'ambito','indole', 'jesus', 'vereda', 'apogeu']
 
-
-
+    
     # ===== Loop principal =====
     while state != DONE:
-        mensagem= f'{ponto}'
-        texto_foratado= font.render(mensagem,True,WHITE)
+        formatando_ponto= f'{ponto}'
+        texto_formatado= font.render(formatando_ponto,True,WHITE)
 
         clock.tick(FPS)
-        segundos = int(pygame.time.get_ticks() - last_update)
-        #print(segundos)
+        segundos=int(40-((pygame.time.get_ticks()-last_update)/1000))
+        formatando_segundo= f'{segundos}'
+        segundotela=font.render(formatando_segundo,True,WHITE)
 
         # ----- Trata eventos
         for event in pygame.event.get():
@@ -49,9 +50,22 @@ def game_screen(window):
                 state = DONE
             if event.type == pygame.KEYDOWN:
                 palavra+=event.unicode
-        if caixa.rect.y>76:
+        if caixa.rect.y>700:
             if palavra== caixa.palavra:
                 ponto+=1
+            else:
+                caixa.speedy+=2
+            caixa.rect.x = random.randint(0,477)
+            palavra=""
+            caixa.rect.y =-76
+            caixa.palavra= random.choice(listapalavra)
+            #caixa.palavra= random.choice(caixa.listapalavra)
+            text = font.render(caixa.palavra  ,True,(0, 0, 0))
+        if segundos ==0:
+            state=DONE
+        if caixa.speedy==30:
+            state=DONE
+
         # ----- Gera saídas
         window.fill(BLACK)  # Preenche com a cor branca
         lista_caixas.draw(window)
@@ -59,11 +73,13 @@ def game_screen(window):
 
         #palavra que o jogador digita
         font = pygame.font.SysFont(None, 48)
-        text2 = font.render(palavra  ,True,(0, 0, 0))
+        text2 = font.render(palavra,True,(0, 0, 0))
         window.blit(text2, (caixa.rect.x+70, caixa.rect.y+35))
         lista_caixas.update()
         #printa placar
-        window.blit(texto_foratado,(30,10))
+        window.blit(texto_formatado,(30,10))
+        #printa segundo
+        window.blit(segundotela,(830,10))
 
         pygame.display.update()  # Mostra o novo frame para o jogador
 
--- sprites.py
+++ sprites.py
@@ -11,14 +11,15 @@ class Caixa(pygame.sprite.Sprite):
         self.image = dicionario_de_arquivos['input'] # Carregue a imagem do input disponível em dicionario_de_arquivos
         self.image= pygame.transform.scale(self.image, (273,76))
         self.rect = self.image.get_rect()
-        self.palavra= "teste"
+        listapalavra=['exceto', 'cinico',"banana","bala","batata","bateria","cacau","cadeado", "cadeira", "cafe" ,"cafune","domado","domino","doze","duvida","duzia", 'idoneo', 'ambito', 'nescio', 'exceto', 'cinico', 'idoneo', 'ambito','indole', 'jesus', 'vereda', 'apogeu']
+        self.palavra= random.choice(listapalavra)
         self.rect.x = random.randint(0,477)
         self.rect.y =-76
-        self.speedy = 10
+        self.speedy = 3
 
     def update(self):
         #Atualização da posição da nave
-        self.rect.y+=2
+        self.rect.y+= self.speedy
         #Aqui você deve definir a lógica para fazer a caixa cair
 
 
